644bf0da5878d61d2d05824763e7095bfddce604
ikoindra <ikoindra91@gmail.com>
2025-09-09 14:22:11 +0700

    backend: auth

--- edu-app/app/Http/Controllers/UserController.php
+++ edu-app/app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 use App\Models\User;
 
@@ -16,5 +17,60 @@ class UserController extends Controller
             'success' => true,
             'data' => $users
         ]);
+    }
+
+    public function show($id)
+    {
+        // Ambil data user berdasarkan ID
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'User not found'], 404);
+        }
+        return response()->json(['success' => true, 'data' => $user]);
+    }
+
+    public function store(Request $request)
+    {
+        //Membuat user baru
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password 
+            ]);
+            
+            return response()->json(['success' => true, 'data' => $user], 201);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Perbarui data user berdasarkan ID
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'User not found'], 404);
+        }
+
+        $user->update([
+            'name' => $request->name ?? $user->name,
+            'email' => $request->email ?? $user->email,
+            'password' => $request->password ? Hash::make($request->password) : $user->password
+        ]);
+
+        return response()->json(['success' => true, 'data' => $user]);
+    }
+
+     public function destroy($id)
+    {
+        // Hapus user berdasarkan ID
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'User not found'], 404);
+        }
+
+        $user->delete();
+        return response()->json(['success' => true, 'message' => 'User deleted']);
     }
 }
--- edu-app/routes/api.php
+++ edu-app/routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
+
+// user routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/users', [UserController::class, 'index']);          
+Route::get('/users/{id}', [UserController::class, 'show']);      
+Route::post('/users', [UserController::class, 'store']);         
+Route::put('/users/{id}', [UserController::class, 'update']);    
+Route::delete('/users/{id}', [UserController::class, 'destroy']); 
 
 // CRUD routes for books
 Route::get('/books', [BookController::class, 'index']);
